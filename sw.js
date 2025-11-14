b1e21a3cbd19c394535bbe68dbec4937c4b8f76c
sener-king <sener86@gmx.de>
2025-11-14 23:35:03 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'sener-winkel-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache geöffnet');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
